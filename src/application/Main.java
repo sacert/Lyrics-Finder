771ec14e5385ac417c5ced0f1ab7638269c61beb
stephen <stephenkang9@gmail.com>
2016-01-02 05:09:52 -0800

    Window bug fix

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,6 +8,8 @@ import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.fxml.FXMLLoader;
 
 
@@ -20,8 +22,14 @@ public class Main extends Application {
 			Pane root = (Pane)FXMLLoader.load(getClass().getResource("FX.fxml"));
 			Scene scene = new Scene(root,484,193);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Rectangle rect = new Rectangle(484, 193);
+			rect.setArcHeight(10);
+			rect.setArcWidth(10);
+			root.setClip(rect);
 			primaryStage.setScene(scene);
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			scene.setFill(Color.TRANSPARENT);
 			primaryStage.show();
 
 		} catch(Exception e) {
